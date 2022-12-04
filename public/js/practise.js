21b4c7191b4f39c1d72077348db6bf5ac10fd8d4
AleXander FiraXis <ivanescu.alexandru01@gmail.com>
2022-12-04 13:12:34 +0200

    added return in function and finish ex.8

--- public/js/practise.js
+++ public/js/practise.js
@@ -1,27 +1,27 @@
 // Ex 1 dublu primul numar cu functie si parametru//
 
-DobleNumber(1, 2)
-function DobleNumber(number1, number2) {
-    let number = number1 += number2;
+let number = DoubleNumber(12);
+function DoubleNumber(number1) {
+    let result = number1 + number1;
 
-    console.log(number);
+    return result;
 }
-
+console.log(number)
 
 //Ex 2 patratul unui numar cu functie si parametru//
-TheSquareOfaNumbers(2, 2)
-function TheSquareOfaNumbers(num1, num2) {
-    let squareNumber = num1 *= num2;
+let numbers = TheSquareOfaNumbers(2);
+function TheSquareOfaNumbers(num1) {
+    let squareNumber = num1 * num1;
 
-    console.log(squareNumber);
+    return squareNumber;
 }
-
+console.log(numbers);
 
 //Ex 3 cel mai mare numar cu functie cu if else si parametru//
 
-CompareNumberIfElse(5, 3)
+let compare = CompareNumberIfElse(5, 3);
 function CompareNumberIfElse(number1, number2) {
-    let result = "answer is "
+    let result = "answer is ";
 
     if (number1 > number2) {
         result = "answer is " + number1;
@@ -43,13 +43,13 @@ function factorial(n) {
         return n * factorial(n - 1);
     }
 }
-let n = 5
+let n = 5;
 answer = factorial(n);
 
 console.log(answer);
 
 //Ex 5 numarul de litere cu ajutorul unei functii cu parametru//
-numbersOfLetters("ma-ta")
+let letters = numbersOfLetters("ma-ta");
 function numbersOfLetters(text) {
 
     let letter = text.length;
@@ -71,11 +71,33 @@ function countVowel(str) {
         }
     }
 
-    return count
+    return count;
 }
 
-const string = prompt('Enter your world: ');
+const string = prompt('Enter your word to count vowels: ');
 
 const result = countVowel(string);
 
 console.log(result);
+
+// Ex 7 numarul de consoane dintru-un cuvant cu functie si parametru //
+
+const consonant = ["b", "c", "d", "f", "g", "j", "k", "l", "m", "n", "p", "q", "s", "t", "v", "x", "z", "h", "r", "w", "y"]
+
+function countConsonant(str) {
+
+    let count = 0;
+    for (let letter of str.toLowerCase()) {
+        if (consonant.includes(letter)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+const String = prompt('Introduce your word to count consonant: ');
+
+const Result = countConsonant(string);
+
+console.log(Result);
